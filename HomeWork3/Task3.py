0235cdd2ea3108eccf751b27d59edb682b5db4ea
Andrey Drozdov <drozdov_andrey@list.ru>
2022-09-12 17:39:14 +0300

    added task3

--- HomeWork3/Task3.py
+++ HomeWork3/Task3.py
@@ -4,3 +4,15 @@
 
 # - [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
 #  - [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
+
+def difference_max_min(list_of_numbers):
+    i = 0
+    while i < len(list_of_numbers):
+        list_of_numbers[i] = round(list_of_numbers[i] - (int(list_of_numbers[i])),10)
+        i += 1
+    difference_number = round((max(list_of_numbers) - min(list_of_numbers)), 10)
+    print(difference_number)
+
+
+list_of_numbers = [1.1, 1.2, 3.1, 5.17, 10.02]
+difference_max_min(list_of_numbers)
